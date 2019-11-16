fc7c453be06bf705d3d60058923800338d919237
Vadim Manaenko <vadim.razorq@gmail.com>
2019-11-16 19:28:16 +0300

    Update

--- src/raspberry_pi_led/led.py
+++ src/raspberry_pi_led/led.py
@@ -1,4 +1,5 @@
 import rospy
+import socket
 from std_msgs.msg import String
 
 class Led:
@@ -8,10 +9,13 @@ class Led:
         '''
         rospy.init_node('raspberry_pi_led')
         rospy.loginfo("I'm alive");
+        clientsocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        clientsocket.connect(('raspberrypi', 8089))
         
 
         def callback(data):
             rospy.loginfo("I got the task");
+            clientsocket.send(bytes('r', 'UTF-8'))
 
         rospy.Subscriber('/task', String, callback)
 
